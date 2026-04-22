787d8a459c5e77227eb517338b8b335b990b897a
Alexander <pchp250g8@tutamail.com>
2026-04-22 12:51:53 +0300

    Fixed Source Code

--- CountSymbolOfString/Program.cs
+++ CountSymbolOfString/Program.cs
@@ -2,7 +2,7 @@
 var strLine = Console.ReadLine();
 Console.WriteLine("Input a symbol");
 var c = 0;
-var chSym = Console.Read();
+var chSym = (char)Console.Read();
 var nLen = strLine.Length;
 for (int i = 0; i < nLen; i++)
 {
@@ -12,9 +12,9 @@ for (int i = 0; i < nLen; i++)
     }
 }
 var fltPart = ((float)c / nLen) * 100f;
-Console.WriteLine("The symbol {0} occurs {1} times", (char)chSym, c);
+Console.WriteLine("The symbol {0} occurs {1} times", chSym, c);
 Console.WriteLine
 (
     "The symbol \"{0}\" occupies {1}% of the string \"{2}\"",
-    (char)chSym, fltPart, strLine
+    chSym, fltPart, strLine
 );
